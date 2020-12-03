dd430f4c8f675842cb9396bd0ad39ceec34b5d0a
k5000455 <k5000455@C-A2203-00>
2020-12-03 11:50:29 +0200

    Add project files.

---
+++ MQTT_Pub_Aute18sa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace MQTT_Pub_Aute18sa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MqttClient client = new MqttClient("127.0.0.1");
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            while (true)
+            {
+                Random rnd = new Random();
+                int r = rnd.Next(18, 22);
+                string str = r.ToString();
+                Console.WriteLine("lähetetiin: " + str);
+                Console.WriteLine("kuitattiin: "+client.Publish("/A140.2/lampo",Encoding.UTF8.GetBytes(str),1,false));
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
